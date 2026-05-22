1c2f89f1787ff6a7dfe0eef4d7a0278475f7febe
Wojciech Mokwiński <79480681+wmcqueensky@users.noreply.github.com>
2026-05-22 16:55:52 +0200

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,22 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.keys = []
+
+    def add(self, key: int) -> None:
+        if key not in self.keys:
+            self.keys.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.keys:
+            self.keys.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.keys
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
